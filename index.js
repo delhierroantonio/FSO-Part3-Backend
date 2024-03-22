7a7ce98548d5f14e69a9d98b7efd20b5b931f671
antoniodelhierro <delhierroantonio@icloud.com>
2024-03-21 22:21:56 -0600

    finish step 1 from phonebook backend-3.1

--- index.js
+++ index.js
@@ -1,2 +1,42 @@
 const express = require('express')
 const app = express()
+
+const PORT = 3001
+
+const persons = [
+  { 
+    "id": 1,
+    "name": "Arto Hellas", 
+    "number": "040-123456"
+  },
+  { 
+    "id": 2,
+    "name": "Ada Lovelace", 
+    "number": "39-44-5323523"
+  },
+  { 
+    "id": 3,
+    "name": "Dan Abramov", 
+    "number": "12-43-234345"
+  },
+  { 
+    "id": 4,
+    "name": "Mary Poppendieck", 
+    "number": "39-23-6423122"
+  }
+]
+
+// # get all persons request
+app.get('/api/persons/', (req, res) => {
+  const data = res.json(persons)
+})
+
+// # get single contact
+// app.get('/api/person/:id', (req, res) => {
+
+// })
+
+// # app liste port
+app.listen(PORT, () => {
+  console.log(`App running on PORT: ${PORT}`);
+})
