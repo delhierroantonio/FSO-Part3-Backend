d6da3c32e7a356c118ceff52329799bc0ebbfac6
antonio del hierro <antoniodelhierro@antonios-Mac-mini.local>
2024-07-01 17:55:04 -0600

    fix: README.md file to it's correct name

--- index.js
+++ index.js
@@ -92,7 +92,7 @@ app.delete('/api/persons/:id', (req, res) => {
 
 // #05 create new entry request
 app.post('/api/persons', (req, res) => {
-  console.log('my log: ', req.body.phone)
+  // console.log('my log: ', req.body.phone)
   const generateId = () => {
     return Date.now().toString(36) + Math.random().toString(36).substr(2)
   }
